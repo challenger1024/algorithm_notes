b414a238002df797e40f23fc8dc5e2858d83606b
challenger1024 <127417636+challenger1024@users.noreply.github.com>
2024-03-30 09:34:14 +0800

    add solution

--- LeetCode/problem-everyday/2024-03-29#2962-1.py
+++ LeetCode/problem-everyday/2024-03-29#2962-1.py
@@ -23,3 +23,20 @@ s=Solution()
 nums = [1,3,2,3,3] 
 k = 2
 print(s.countSubarrays(nums,k))
+
+#Solution
+'''Divided into zones [l,r], the outer layer is convenient r, the inner layer is convenient l.
+class Solution:
+	def countSubarrays(self, nums, k: int) -> int:
+		l=0
+		ans=0
+		cnt=0#counter of max value
+		value=max(nums)#calculate the max value 
+		for num in nums:#num is nums[r],I don't use index r
+			if num==value: cnt+=1#nums[r]=max_value,counter+=1
+			while cnt==k:#When max_value occurs k times in the interval [l,r], the left pointer is moved
+				if nums[l]==value:cnt-=1#nums[l]==max_value,counter-=1
+				l+=1
+			ans+=l#When the left pointer is ending of moving, the position of the left pointer is just the next index of l  in the interval [l,r] that meets the condition of the problem, and all the subarrays with the left end point of l meet the condition, so the answer is updated
+		return ans
+'''
